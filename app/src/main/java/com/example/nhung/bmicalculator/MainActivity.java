109ad215cf504f16f537b2ba23f013ff2888d739
qnguyen29 <qnnguyen29@gmail.com>
2016-10-01 16:55:14 -0400

    layout wth radio button

--- app/src/main/java/com/example/nhung/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/nhung/bmicalculator/MainActivity.java
@@ -12,37 +12,33 @@ public class MainActivity extends AppCompatActivity {
     TextView ans;
     EditText weightBox;
     EditText heightBox;
-    //EditText poundsTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         weightBox = (EditText) findViewById(R.id.weightBox);
         heightBox = (EditText) findViewById(R.id.heightBox);
-        //poundsTxt = (EditText) findViewById(R.id.poundTxt);
         ans = (TextView) findViewById(R.id.ansBox);
-
-
         Button calculator = (Button) findViewById(R.id.calBut);
+
         calculator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 float feet = Float.parseFloat(heightBox.getText().toString());
-                //double inches = Float.parseFloat(inchesTxt.getText().toString());
                 float weight = Float.parseFloat(weightBox.getText().toString());
 
-                //double totalHeight = (feet * 12) + inches;
                 double bmi = (weight * 703) / Math.pow((feet * 12), 2);
-//                double bmi = (weight * 703) / Math.pow((feet ), 2);
-                System.out.println(bmi);
                 ans.setText(String.format("%.1f", bmi));
 
             }
-
         });
+
+
+        //radio group
+        Button weightSelected = (Button) findViewById(R.id.weightBox);
 
     }
 }
