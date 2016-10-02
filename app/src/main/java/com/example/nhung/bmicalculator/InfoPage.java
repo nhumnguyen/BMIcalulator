e51dd3b5dc35710a6e765ccc380ddb04afdfc4ea
qnguyen29 <qnnguyen29@gmail.com>
2016-10-02 02:43:26 -0400

    info page added

--- app/src/main/java/com/example/nhung/bmicalculator/InfoPage.java
+++ app/src/main/java/com/example/nhung/bmicalculator/InfoPage.java
@@ -1,13 +1,25 @@
 package com.example.nhung.bmicalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InfoPage extends AppCompatActivity {
 
-    @Override
+    TextView bmi;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
+
+
+        Intent intent = getIntent();
+        double value = intent.getDoubleExtra("bmi", 0);
+        bmi = (TextView) findViewById(R.id.bmiBox);
+        bmi.setText(String.format("%.1f",value));
+
+
+
     }
 }
