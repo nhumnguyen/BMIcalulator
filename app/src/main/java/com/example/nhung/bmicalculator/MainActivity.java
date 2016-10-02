031791547868dbea46d5480e9ff85304cbc4a43c
qnguyen29 <qnnguyen29@gmail.com>
2016-10-02 01:24:06 -0400

    infoPage activity

--- app/src/main/java/com/example/nhung/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/nhung/bmicalculator/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.nhung.bmicalculator;
 
-import android.provider.MediaStore;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -25,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
     float feet;
     float weight;
 
+    ImageButton info;
+    Button calculator;
+
+    double bmi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +40,12 @@ public class MainActivity extends AppCompatActivity {
         heightBox = (EditText) findViewById(R.id.heightBox);
 
         ans = (TextView) findViewById(R.id.ansBox);
-        Button calculator = (Button) findViewById(R.id.calBut);
+        calculator = (Button) findViewById(R.id.calBut);
 
 
         //radio group
         weightgroup = (RadioGroup) findViewById(R.id.weightgroup);
         heightgroup = (RadioGroup) findViewById(R.id.heightgroup);
-        //calBut = (Button) findViewById(R.id.calBut);
 
         calculator.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,13 +70,34 @@ public class MainActivity extends AppCompatActivity {
                 weight = Float.parseFloat(weightBox.getText().toString());
                 converter(weighttype, heighttype);
 
-                double bmi = (weight * 703) / Math.pow((feet * 12), 2);
+                bmi = (weight * 703) / Math.pow((feet * 12), 2);
                 ans.setText(String.format("%.1f", bmi));
                 System.out.println(bmi);
 
+
             }
         });
+
+
+
+
+        //go to new page from image button
+        info = (ImageButton) findViewById(R.id.info);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //navigate to new activity
+                Intent showInfo = new Intent(MainActivity.this, InfoPage.class);
+                showInfo.putExtra("bmi", bmi); //passing value
+                MainActivity.this.startActivity(showInfo);
+            }
+        });
+
     }
+
+
+
+
 
     public void converter(String weighttype, String heighttype) {
         if (weighttype.equals("kg")) {
